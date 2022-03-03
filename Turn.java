8ce1f06000209e4969e97d0f42a7e59f463ecee9
hawkalex2001 <91161373+hawkalex2001@users.noreply.github.com>
2022-03-02 22:13:58 -0500

    Add files via upload

---
+++ Turn.java
@@ -0,0 +1,15 @@
+public class Turn {
+    private int turnCount;
+
+    public void Turn(int turnCount){
+        //counts number of turns
+    }
+
+    public void playerTurn(){
+        //initiates the players turn
+    }
+
+    public void enemyTurn(){
+        //initiates the enemies turn
+    }
+}
