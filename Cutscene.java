e2d0d121b71c330f3bb53defd419f7a0702fd452
Jacob Klucher <90862594+klucher@users.noreply.github.com>
2022-03-03 11:55:21 -0500

    Add files via upload

---
+++ Cutscene.java
@@ -0,0 +1,62 @@
+public class Cutscene
+{
+    // Intro cutscene describing the setting and the goal of the player as well as the exterior of the castle
+    public String cutscene1()
+    {
+        return "";
+    }
+
+    // Describing the castle interior after defeating the first boss
+    public String cutscene2()
+    {
+        return "";
+    }
+
+    // Describes entering the kitchen as well as intro to the kitchen boss
+    public String cutscene3()
+    {
+        return "";
+    }
+
+    // Describes the office as well as the office boss
+    public String cutscene4()
+    {
+        return "";
+    }
+
+    // Description of going upstairs and hallway
+    public String cutscene5()
+    {
+        return "";
+    }
+
+    // Description of hallway leading to "Serverus" boss
+    public String cutscene6()
+    {
+        return "";
+    }
+
+    // Description of final boss room and final boss
+    public String cutscene7()
+    {
+        return "";
+    }
+
+    // Description of Filezilla boss
+    public String specialCutscene1()
+    {
+        return "";
+    }
+
+    // Going left three times in hallway leads to consumable event
+    public String specialCutscene2()
+    {
+        return "";
+    }
+
+    // End cutscene, "you've defeated the boss and saved the world yayy!!"
+    public String endCutscene1()
+    {
+        return "";
+    }
+}
---
+++ DirectionChoice.java
@@ -0,0 +1,34 @@
+public class DirectionChoice
+{
+    public void directionChoice0()
+    {
+        // Choice of opening castle door or turning around and leaving
+    }
+    public void directionChoice1()
+    {
+        // After entering the castle describe the hallways going
+        // left and right and then ask player which direction to go
+    }
+
+    public void directionChoice2()
+    {
+        // Describe hallways going left and right and then ask player which direction to go
+    }
+
+    public void directionChoice3()
+    {
+        // Choice of going up the stairs past the defeated boss or look around the office
+    }
+
+    public void directionChoice4()
+    {
+        // Choice of going straight down long hallway, left down another hallway, or right towards a room
+        // Left choice leads back to starting choice of left, straight, or right
+    }
+
+    public void directionChoice5()
+    {
+        // "Choice" of opening the final door to the boss
+        // Not an actual choice because you have to fight the boss
+    }
+}
