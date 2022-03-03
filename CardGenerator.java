a9cf639119fe27322501fbea553aca1ef0b82252
boylec1 <91161196+boylec1@users.noreply.github.com>
2022-03-02 19:45:06 -0500

    Add files via upload
    Did these according to the UML, but had to change a few things (method "randomStats" to not take in arguments, as it's a randomizer, changed method "updateHealth" to a void, as it's a setter).
    
    Might play with them a bit more and expand them out a bit beyond what they are, but they're workable for now.

---
+++ CardGenerator.java
@@ -0,0 +1,18 @@
+public class CardGenerator {
+
+    // Fields
+    private int cardAttack;
+    private int cardDefense;
+    private String defenseAttribute;
+
+    public CardGenerator(int cardAttack, int cardDefense)
+    {
+        // Constructor setting the attack and defense values of a generated card
+    }
+
+    public void randomStats()
+    {
+        // Assigns random values for attack and defense to a card generated
+    }
+
+}
---
+++ Combat.java
@@ -0,0 +1,25 @@
+public class Combat {
+
+    // Fields
+    private int attackInput;
+    private int defenseInput;
+    private String attackAttribute;
+    private String defenseAttribute;
+
+    public Combat(int attackInput, int defenseInput, String attackAttribute, String defenseAttribute)
+    {
+        // Constructor to set initial values
+    }
+
+    public String output()
+    {
+        // Reports the result of combat between player and enemy
+        return null;
+    }
+
+    public void updateHealth(int newHealth)
+    {
+        // Sets health to a new value
+    }
+
+}
