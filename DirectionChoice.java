49e19c14e167e6a8198039b050a20d0b6abeda61
boylec1 <91161196+boylec1@users.noreply.github.com>
2022-03-06 23:43:05 -0500

    Update DirectionChoice.java
    First part of this. Tested it to make sure it works, the story elements can obviously change but I tried to follow the lovely intro developed in Cutscene.java.

--- DirectionChoice.java
+++ DirectionChoice.java
@@ -1,8 +1,37 @@
+import java.util.*;
+
 public class DirectionChoice
 {
     public void directionChoice0()
     {
         // Choice of opening castle door or turning around and leaving
+        Scanner kb = new Scanner(System.in);
+        // Choice of opening castle door or turning around and leaving
+        System.out.println("\nAs the Bot Lord's castle looms before you, you have a decision to make: enter, " +
+                "or leave?");
+
+        String choice = kb.nextLine();
+
+        while(!choice.equalsIgnoreCase("enter") && !choice.equalsIgnoreCase("leave"))
+        {
+            System.out.println("That is not a valid choice. Time waits for no one; choose your fate.");
+            choice = kb.nextLine();
+        }
+
+        if(choice.equalsIgnoreCase("enter"))
+        {
+            System.out.println("You steel yourself and push forward through the door of the castle. You" +
+                    " resolve that the Bot Lord will see his end this day.");
+            DirectionChoice initialDirection = new DirectionChoice();
+            initialDirection.directionChoice1();
+        }
+        else
+        {
+            System.out.println("Your shoulders slump in resignation and fear. You turn toward " +
+                    "the wasteland to meet a slow end. The Bot Lord will continue unopposed.\n");
+            System.out.println("GAME OVER");
+            System.exit(0);
+        }
     }
     public void directionChoice1()
     {
