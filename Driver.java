571315474792707b0015cfb216123572aa51074a
Jacob Klucher <90862594+klucher@users.noreply.github.com>
2022-03-07 14:01:58 -0500

    updated Driver.java

--- Driver.java
+++ Driver.java
@@ -3,8 +3,15 @@ public class Driver
     public static void main(String[] args)
     {
         Cutscene cutscenes = new Cutscene();
-        String name = "";
-        System.out.println(cutscenes.cutscene1(name));
+        DirectionChoice direction = new DirectionChoice();
+
+        System.out.println(cutscenes.cutscene1());
+        System.out.println(cutscenes.enterName());
+        direction.directionChoice0();
+
+        // event / boss fight
+
+        System.out.println(cutscenes.cutscene2());
 
         boolean goFirst = false;
         CoinFlip flip1 = new CoinFlip(goFirst);
