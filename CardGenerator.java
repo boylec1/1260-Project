5486e1b8b8f90aadb719fb7e010fc9be769ea2da
boylec1 <91161196+boylec1@users.noreply.github.com>
2022-03-07 11:14:18 -0500

    Update CardGenerator.java
    Filled out the constructor, as well as an arbitrary random stat generator. Put fill-in methods for names and attributes for later.

--- CardGenerator.java
+++ CardGenerator.java
@@ -1,18 +1,39 @@
+import java.util.*;
+
 public class CardGenerator {
 
     // Fields
     private int cardAttack;
     private int cardDefense;
     private String defenseAttribute;
+    private String cardName;
 
     public CardGenerator(int cardAttack, int cardDefense)
     {
-        // Constructor setting the attack and defense values of a generated card
+        this.cardAttack = cardAttack;
+        this.cardDefense = cardDefense;
+        this.defenseAttribute.equals(defenseAttribute);
+        this.cardName.equals(cardName);
     }
 
     public void randomStats()
     {
-        // Assigns random values for attack and defense to a card generated
+        Random generator = new Random();
+        // Values are just fill-ins, we'll decided on actual values later when the game gets more built.
+        this.cardAttack = generator.nextInt(5) + 1;
+        this.cardDefense = generator.nextInt(5) + 1;
+    }
+    
+    public void randomAttribute()
+    {
+        // this is where we decide on attributes, will pull from a separate file of attributes randomly
+    }
+
+    public void randomName()
+    {
+        // this is where we decide on names, will pull from a separate file of names randomly
+        // something like a few dozen names/titles, numbered, use number generator to pull one
+        // and assign it
     }
 
 }
