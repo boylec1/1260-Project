7f72a2ca78a65d2c4857f35f429f08c6424c3d7a
boylec1 <91161196+boylec1@users.noreply.github.com>
2022-03-08 16:49:28 -0500

    Update Card.java
    Putting the randomizer in here, CardGenerator.java should be either deprecated or repurposed for consumables.

--- Card.java
+++ Card.java
@@ -12,6 +12,18 @@ public class Card
         this.cardAttribute = cardAttribute;
         this.cardName = cardName;
     }
+    
+    public void randomStats()
+    {
+        Random generator = new Random();
+        
+        this.cardAttack = generator.nextInt(5) + 1;
+        this.cardDefense = generator.nextInt(5) + 1;
+        
+        // this is where we decide on attributes, will pull from a separate file of attributes randomly
+        
+        // this is where we decide on names, will pull from a separate file of names randomly
+    }
 
     public String toString() 
     {
