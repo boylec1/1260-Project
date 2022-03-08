f68675b99cfea8fcc924b0a8e467626b6015289e
boylec1 <91161196+boylec1@users.noreply.github.com>
2022-03-08 00:27:58 -0500

    Update CardGenerator.java
    Condensed the name and attribute generators into the randomStats method. I think it should end up like that anyways, doing it in separate methods just makes less sense the more I look at it. It's all going to have to pass back through Card.java as a whole anyways, I think.

--- CardGenerator.java
+++ CardGenerator.java
@@ -22,18 +22,9 @@ public class CardGenerator {
         
         this.cardAttack = generator.nextInt(5) + 1;
         this.cardDefense = generator.nextInt(5) + 1;
-    }
-    
-    public void randomAttribute()
-    {
+        
         // this is where we decide on attributes, will pull from a separate file of attributes randomly
-    }
-
-    public void randomName()
-    {
+        
         // this is where we decide on names, will pull from a separate file of names randomly
-        // something like a few dozen names/titles, numbered, use number generator to pull one
-        // and assign it
     }
-
 }
