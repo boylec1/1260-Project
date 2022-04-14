fe316ca771f3f0f45a19a12f27a0b54092718861
boylec1 <91161196+boylec1@users.noreply.github.com>
2022-04-13 23:59:58 -0400

    Update Card.java
    Added attribute and name generators. Cleaned up the toString so all the prints are in a vertical line (everything isn't staggered). 
    
    Attribute and Name generators are kind of placeholders; we have an Enum for the former and can make one for the latter, but I'm not yet sure how to randomize from an Enum, so we can fix that later.

--- Card.java
+++ Card.java
@@ -1,11 +1,22 @@
 import java.util.Random;
+
 public class Card
 {
     private int cardAttack;
     private int cardDefense;
     private String cardAttribute;
-    private String cardName;
-
+    private String cardName;  
+    
+    public Card()
+    {
+        Random generator = new Random();
+        
+        this.cardAttack = generator.nextInt(5) + 1;
+        this.cardDefense = generator.nextInt(5) + 1;
+        this.cardAttribute = randomAttribute(generator.nextInt(5)+1);
+        this.cardName = randomName();
+    }
+    
     public Card(int cardAttack, int cardDefense, String cardAttribute, String cardName)
     {
         this.cardAttack = cardAttack;
@@ -14,16 +25,43 @@ public class Card
         this.cardName = cardName;
     }
     
-    public void randomStats()
+    // This is a placeholder. I know we have the Enum for these, but I don't yet know how to randomize from Enums.
+    public String randomAttribute(int thePick)
+    {
+
+        String theAttribute = "";
+
+        switch(thePick){
+            case 1:
+                theAttribute = "Virus";
+                break;
+            case 2:
+                theAttribute = "Antivirus";
+                break;
+            case 3:
+                theAttribute = "Data";
+                break;
+            case 4:
+                theAttribute = "Software";
+                break;
+            case 5:
+                theAttribute = "Hardware";
+                break;
+        }
+
+        return theAttribute;
+    }
+    
+    // this is a placeholder. Might use Enums for this instead, but I still need to figure out how to randomize from them.
+    public String randomName()
     {
         Random generator = new Random();
-        
-        this.cardAttack = generator.nextInt(5) + 1;
-        this.cardDefense = generator.nextInt(5) + 1;
-        
-        // this is where we decide on attributes, will pull from a separate file of attributes randomly
-        
-        // this is where we decide on names, will pull from a separate file of names randomly
+        String[] names = { "Jessica", "Dave", "Sarah", "Steve", "Nancy", "Ron" };
+        String[] descriptors = { " the Destroyer", " the Breaker", ", World-Ender", ", Habitual Sneezer", ", Mean Person", " the Litterer" };
+
+        String theName = names[generator.nextInt(names.length)] + descriptors[generator.nextInt(descriptors.length)];
+
+        return theName;
     }
     
     public int getCardAttack()
@@ -49,10 +87,10 @@ public class Card
     public String toString() 
     {
         String aCard = "";
-        aCard += "Card Name: " + cardName; // need to be able to generate names, if we're going to have names
-        aCard += "Attack Value: " + cardAttack;
-        aCard += "Defense Value: " + cardDefense;
-        aCard += "Special Action: " + cardAttribute;
+        aCard += "Card Name:\t\t" + cardName + "\n"; 
+        aCard += "Attack Value:\t" + cardAttack + "\n";
+        aCard += "Defense Value:\t" + cardDefense + "\n";
+        aCard += "Special Action:\t" + cardAttribute + "\n";
 
         return aCard;
     }
