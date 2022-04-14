e3de4d1d8d77605f0ede1ba2a5d75762bf318acd
Aaryanna18 <71777339+Aaryanna18@users.noreply.github.com>
2022-04-14 09:49:16 -0400

    Update Turn.java

--- Turn.java
+++ Turn.java
@@ -1,15 +1,23 @@
-public class Turn {
+public class Turn
+{
     private int turnCount;
 
-    public void Turn(int turnCount){
+    public void Turn(int turnCount)
+    {
         //counts number of turns
-    }
-
-    public void playerTurn(){
-        //initiates the players turn
-    }
-
-    public void enemyTurn(){
-        //initiates the enemies turn
+        List<Integer> list = new ArrayList<Integer>();
+        List<Integer> runningtotal = new ArrayList<Integer>();
+        runningtotal.add(list.get(0));
+        int prev_sum = list.get(0);
+        int i = 1;
+        while (i < list.size())
+        {
+            prev_sum += list.get(i);
+            runningtotal.add(prev_sum);
+            i++;
+        }
+        System.out.println();
+        System.out.print("Combat took: " + i);
     }
 }
+
