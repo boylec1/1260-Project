deb9d3b0eeb95d7b79234194d703d286a6b15b1e
00gphillips00 <100809607+00gphillips00@users.noreply.github.com>
2022-04-14 10:17:27 -0400

    Update EnemyDeck.java

--- EnemyDeck.java
+++ EnemyDeck.java
@@ -1,19 +1,75 @@
 public class EnemyDeck
 {
-    private Card[] cards;
+    private ArrayList<Card> cards = new ArrayList<>();
     private int deckCapacity;
+    private boolean[] isCardDealt;
+    private int numberOfCards = 0;
 
-    public void Deck(int deckCapacity){
-        //the deck capacity
+    public void Deck()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            Card newCard = new Card();
+            cards.add(newCard);
+        }
+        this.deckCapacity = cards.size();
+        this.isCardDealt = new boolean[cards.size()];
     }
 
-    public void add(Card card){
-        //adds a card to the deck
+    public void enemyDeck(int manualDeckCapacity)
+    {
+        for(int i = 0; i < manualDeckCapacity; i++)
+        {
+            Card newCard = new Card();
+            cards.add(newCard);
+        }
+        this.deckCapacity = cards.size();
+        this.isCardDealt = new boolean[cards.size()];
     }
 
-    public String toString(){
-        //what prints
-        return null;
+    //getter for cards
+    public ArrayList<Card> getCards()
+    {
+        return this.cards;
     }
 
+    //getter for deckCapacity
+    public int getDeckCapacity()
+    {
+        return this.deckCapacity;
+    }
+
+    //getter for isCardDealt
+    public boolean[] getIsCardDealt()
+    {
+        return this.isCardDealt;
+    }
+
+    //getter for numberOfCards
+    public int getNumberOfCards()
+    {
+        return this.numberOfCards;
+    }
+
+    public void add(Card card)
+    {
+        cards.add(card);
+    }
+
+    public void dealCard(int index)
+    {
+        //returns card at index if true
+        isCardDealt[index] = true;
+    }
+
+    public void returnToDeck(int index)
+    {
+        // sets isCardDealt at an index to false
+        isCardDealt[index] = false;
+    }
+
+    public void shuffleCards(Card cards)
+    {
+        //randomly shuffles cards in deck
+    }
 }
