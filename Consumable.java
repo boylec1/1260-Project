a38c98eabe2566b24a7ab64b865a97c87383e399
boylec1 <91161196+boylec1@users.noreply.github.com>
2022-04-14 10:45:14 -0400

    Added Consumable.java
    Adding a file for consumables to replace CardGenerator.java. May not use it but it'll be here for now if we do.

---
+++ Consumable.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Consumable {
+
+    // Fields
+    private String itemOne = "HI! I'M A PLACEHOLDER! MOO!";
+    private String itemTwo = "HI! I'M A PLACEHOLDER! MOO!";
+    private String itemThree = "HI! I'M A PLACEHOLDER! MOO!";
+
+    public Consumable()
+    {
+        // Some constructor here. Maybe a randomizer? 
+    }
+
+    public void useItemOne()
+    {
+        // This is probably where it'll do something really cool and interact with stuffs
+    }
+
+    public void useItemTwo()
+    {
+        // This is probably where it'll do something really cool and interact with stuffs
+    }
+
+    public void useItemThree()
+    {
+        // This is probably where it'll do something really cool and interact with stuffs
+    }
+}
